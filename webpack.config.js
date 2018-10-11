596e8442b4d6e75827325a4b4a39f26194c815fd
Doug Elkin <doug@drelyn.net>
2018-10-11 16:05:24 -0400

    trying to get babel to ignore test code

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules)/,
+        exclude: /(node_modules|\.test\.js)/,
         loader: 'babel-loader',
         options: {
           presets: [
