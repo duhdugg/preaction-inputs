7b6414c74258cead1ccfe298e30f642f9f797c55
Doug Elkin <doug@drelyn.net>
2018-10-11 15:31:41 -0400

    build for npm packaging

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+let path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    Form: './src/Form.js',
+    Checkbox: './src/Checkbox.js',
+    Input: './src/Input.js',
+    Select: './src/Select.js',
+    Textarea: './src/Textarea.js',
+    WysiwygEditor: './src/WysiwygEditor.js'
+  },
+  output: {
+    path: path.resolve('lib'),
+    filename: '[name].bundle.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react'
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: 'css-loader'
+      }
+    ]
+  }
+}
