a3cf05d49fb0dc39cf9a4bfbdd53b3346cbf1ddb
Doug Elkin <doug@drelyn.net>
2018-10-15 13:16:38 -0400

    is this what I need?

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-import Checkbox from './dist/Checkbox.dist.js'
-import Form from './dist/Form.dist.js'
-import Input from './dist/Input.dist.js'
-import Select from './dist/Select.dist.js'
-import Textarea from './dist/Textarea.dist.js'
-import Wysiwyg from './dist/Wysiwyg.dist.js'
-
-export default {
-  Checkbox,
-  Form,
-  Input,
-  Select,
-  Textarea,
-  Wysiwyg
-}
+export { default as Checkbox } from './dist/Checkbox.dist.js'
+export { default as Form } from './dist/Form.dist.js'
+export { default as Input } from './dist/Input.dist.js'
+export { default as Select } from './dist/Select.dist.js'
+export { default as Textarea } from './dist/Textarea.dist.js'
+export { default as Wysiwyg } from './dist/Wysiwyg.dist.js'
