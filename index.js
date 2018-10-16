115841352b879cedf83fca5c3431eda3dfa7e0a1
Doug Elkin <doug@drelyn.net>
2018-10-15 22:05:52 -0400

    fixed expected require import behavior

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-export { default as Checkbox } from './dist/Checkbox.min.js'
-export { default as Form } from './dist/Form.min.js'
-export { default as Input } from './dist/Input.min.js'
-export { default as Select } from './dist/Select.min.js'
-export { default as Textarea } from './dist/Textarea.min.js'
-export { default as Wysiwyg } from './dist/Wysiwyg.min.js'
+import Checkbox from './dist/Checkbox.min.js'
+import Form from './dist/Form.min.js'
+import Input from './dist/Input.min.js'
+import Select from './dist/Select.min.js'
+import Textarea from './dist/Textarea.min.js'
+import Wysiwyg from './dist/Wysiwyg.min.js'
+
+module.exports = {
+  Checkbox,
+  Form,
+  Input,
+  Select,
+  Textarea,
+  Wysiwyg
+}
+module.exports.default = module.exports
