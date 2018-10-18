db5538b6ca15edda897820803ede805cbf10d2d4
Doug Elkin <doug@drelyn.net>
2018-10-18 13:46:54 -0400

    reference default for imports

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const Checkbox = require('./dist/Checkbox.min.js')
-const Form = require('./dist/Form.min.js')
-const Input = require('./dist/Input.min.js')
-const Select = require('./dist/Select.min.js')
-const Textarea = require('./dist/Textarea.min.js')
-const Wysiwyg = require('./dist/Wysiwyg.min.js')
+const Checkbox = require('./dist/Checkbox.min.js').default
+const Form = require('./dist/Form.min.js').default
+const Input = require('./dist/Input.min.js').default
+const Select = require('./dist/Select.min.js').default
+const Textarea = require('./dist/Textarea.min.js').default
+const Wysiwyg = require('./dist/Wysiwyg.min.js').default
 
 module.exports = {
   Checkbox,
