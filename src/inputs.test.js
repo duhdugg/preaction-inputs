44858de629288061ce8c390b32c6e5a0f7f2dc60
Doug Elkin <doug@dougelkin.com>
2020-07-30 19:15:02 -0400

    removed unnecessary tests

--- src/inputs.test.js
+++ src/inputs.test.js
@@ -3,17 +3,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
-import { Input } from '../dist/preaction-inputs.all.min.js'
-import StandaloneInput from '../dist/preaction-inputs.input.min.js'
-const assert = require('assert')
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<App />, div)
   ReactDOM.unmountComponentAtNode(div)
-})
-
-it('imports as ES module', () => {
-  assert(Input !== undefined)
-  assert(StandaloneInput !== undefined)
 })
